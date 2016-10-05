b2df2505be1d97450c5220e1d9c3de4c431938b9
wjm2202 <wjm2202@aut.ac.nz>
2016-10-05 19:42:36 +1300

    make test class for database for eric

---
+++ src/database/NameTest.java
@@ -0,0 +1,54 @@
+package database;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Liandri on 5/10/2016.
+ */
+public class NameTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getNickname() throws Exception {
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void setNickname() throws Exception {
+
+    }
+
+    @Test
+    public void getEmail() throws Exception {
+
+    }
+
+    @Test
+    public void setEmail() throws Exception {
+
+    }
+
+    @Test
+    public void insert() throws Exception {
+
+    }
+
+    @Test
+    public void maketable() throws Exception {
+
+    }
+
+}
